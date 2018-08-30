76551817bdbc9d7aa2114abf084afa9a08fb5029
alen-mujkanovic <alen.mujkanovic@outlook.com>
2018-08-30 11:52:48 -0400

    Improved documentation

--- utils/energy.m
+++ utils/energy.m
@@ -1,5 +1,8 @@
 function E = energy (x)
-% Returns the energy content of the signal
+% Returns the energy content of the input signal as per Parseval's Theorem
+%
+% Input:	x 	Signal in either time or frequency domain
+% Output: 	E 	Energy of the signal
 
 	N = numel(x);
 	E = sum(abs(x.^2)) / N;
--- utils/timeShift.m
+++ utils/timeShift.m
@@ -2,9 +2,9 @@ function output = timeShift(input, dt, delay)
 % timeShift delays a frequency domain input by specified time amount
 % through multiplication with an appropriate delaying signal
 %
-% Inputs:	input	signal which should be shifted in frequency domain
-%			delay	delay time in [s] (advance signal with negative value)
-%			dt		input signal dwell time
+% Inputs:	input	Frequency domain signal to be shifted
+%			delay	Delay time [s]
+%			dt		Input signal dwell time [s]
 
 N = length (input);
 df = 1 / (N*dt);
