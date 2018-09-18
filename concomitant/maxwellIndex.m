792ec5e37b2f61a0c2193bfac0c9d0333d13fd88
alen-mujkanovic <alen.mujkanovic@outlook.com>
2018-09-18 18:03:41 -0400

    Rotationally-invariant Maxwell index

--- concomitant/maxwellIndex.m
+++ concomitant/maxwellIndex.m
@@ -1,5 +1,5 @@
-function m = maxwellIndex(G, p, convex)
-% Calculates a convex alternative to the Maxwell index as defined by 
+function m = maxwellIndex(G, p, type)
+% Calculates a simplified alternative to the Maxwell index as defined by 
 % Szczepankiewicz et al., ISMRM 2018 (goo.gl/vVGQq2), which provides a
 % measure of concomitant field contribution to gradient encoding waveforms.
 %
@@ -7,21 +7,24 @@ function m = maxwellIndex(G, p, convex)
 %			p		Parameter struct
 %			  dir		Normalized encoding direction vector [x y z]
 %			  dt		Time step [s]
-%			  convex	Convexity flag [Bool]
+%			  type	Index type [0,1,2]
 %
-% Outputs:	m		Maxwell index [s*T/m] ([s*T^2/m^2] for non-convex)
+% Outputs:	m		Maxwell index [s*T^2/m^2]
 
 
 if nargin < 3
-	convex = true;
+	type = 0;
 end
 
 % Calculate index
 G = G * p.encodeDir;			% Project gradient onto axes
 invG = invertFields(G,p.inv);	% Invert gradient pulse
 M = G.' * invG * p.dt;			% Integrate gradient squared matrix
-if convex
-	m = sqrt(sum(M(:)));		% Convex maxwell index
-else
-	m = sqrt(trace(M*M'));		% Standard maxwell index using Frobenius norm
+switch type
+	case 0
+		m = trace(M);			% Simplified Maxwell index
+	case 1
+		m = sqrt(trace(M*M.'));	% Maxwell index using Frobenius norm
+	case 2
+		m = sum(G.*invG)*p.dt;	% Diect integration
 end
