c32ec0c6af8496349dff0a1af0c234e176247fac
alen-mujkanovic <alen.mujkanovic@outlook.com>
2018-09-18 18:05:14 -0400

    Added documentation and annotations

--- results/plotGradients.m
+++ results/plotGradients.m
@@ -17,6 +17,7 @@ coco.tVec = (0:coco.n-1) * dt * 1e3;					% [ms]
 subplot(2,3,1:3, 'Units', 'normalized', 'Position', [0.13 0.575 0.775 0.325]);
 title({['Sym: ' sym.info], ['Asym: ' asym.info], ['Coco: ' coco.info]});
 
+% Plot lines
 hold on;
 plot(sym.tVec, sym.G*1e3, 'LineWidth',4);
 plot(asym.tVec, asym.G*1e3, 'LineWidth',4);
@@ -26,23 +27,28 @@ plot(asym.tInv*1e3,0,'kx');
 plot(coco.tInv*1e3,0,'kx');
 hold off;
 
+% Add labels
 xlabel('Time [ms]');
 ylabel('Amplitude [mT/m]');
 legend('G_{sym}','G_{asym}','G_{coco}');
 set(gca, 'FontSize', 16);
 
+
 %% Plot M0
 subplot(2,3,4);
 title('M_0 Evolution','FontSize', 20);
 
+% Plot lines
 hold on;
 plot(sym.tVec, sym.M(1,:), 'LineWidth',4);
 plot(asym.tVec, asym.M(1,:), 'LineWidth',4);
 plot(coco.tVec, coco.M(1,:), 'LineWidth',4);
 hold off
 
+% Add annotations
 yr = max(yticks)-min(yticks);
 y1 = min(yticks)+0.20*yr; y2 = min(yticks)+0.12*yr; y3 = min(yticks)+0.04*yr;
+
 mText = ['$\sigma_{sym}=' num2str(sym.stdM(1),'%1.3g') ' \frac{rad}{m}$'];
 text(1,y1, mText, 'Interpreter','latex', 'FontSize',16)
 mText = ['$\sigma_{asym}=' num2str(asym.stdM(1),'%1.3g') ' \frac{rad}{m}$'];
@@ -50,52 +56,63 @@ text(1,y2, mText, 'Interpreter','latex', 'FontSize',16)
 mText = ['$\sigma_{coco}=' num2str(coco.stdM(1),'%1.3g') ' \frac{rad}{m}$'];
 text(1,y3, mText, 'Interpreter','latex', 'FontSize',16)
 
+% Add labels
 xlabel('Time [ms]');
 ylabel('Moment [rad/m]');
 set(gca, 'FontSize', 16);
+
 
 %% Plot M1
 subplot(2,3,5);
 title('M_1 Evolution','FontSize', 20);
 
+% Plot lines
 hold on;
 plot(sym.tVec, sym.M(2,:), 'LineWidth',4);
 plot(asym.tVec, asym.M(2,:), 'LineWidth',4);
 plot(coco.tVec, coco.M(2,:), 'LineWidth',4);
 hold off
 
+% Add annotations
 yr = max(yticks)-min(yticks);
 y1 = min(yticks)+0.20*yr; y2 = min(yticks)+0.12*yr; y3 = min(yticks)+0.04*yr;
-mText = ['$\sigma_{sym}=' num2str(sym.stdM(2),'%1.3g') ' \frac{rad}{m}$'];
+
+mText = ['$\sigma_{sym}=' num2str(sym.stdM(2),'%1.3g') ' \frac{rad}{m/s}$'];
 text(1,y1, mText, 'Interpreter','latex', 'FontSize',16)
-mText = ['$\sigma_{asym}=' num2str(asym.stdM(2),'%1.3g') ' \frac{rad}{m}$'];
+mText = ['$\sigma_{asym}=' num2str(asym.stdM(2),'%1.3g') ' \frac{rad}{m/s}$'];
 text(1,y2, mText, 'Interpreter','latex', 'FontSize',16)
-mText = ['$\sigma_{coco}=' num2str(coco.stdM(2),'%1.3g') ' \frac{rad}{m}$'];
+mText = ['$\sigma_{coco}=' num2str(coco.stdM(2),'%1.3g') ' \frac{rad}{m/s}$'];
 text(1,y3, mText, 'Interpreter','latex', 'FontSize',16)
 
+% Add labels
 xlabel('Time [ms]');
 ylabel('Moment [rad/m/s]');
 set(gca, 'FontSize', 16);
+
 
 %% Plot M2
 subplot(2,3,6);
 title('M_2 Evolution', 'FontSize', 20);
 
+% Plot lines
 hold on;
 plot(sym.tVec, sym.M(3,:), 'LineWidth',4);
 plot(asym.tVec, asym.M(3,:), 'LineWidth',4);
 plot(coco.tVec, coco.M(3,:), 'LineWidth',4);
 hold off
 
+% Add annotations
 yr = max(yticks)-min(yticks);
 y1 = min(yticks)+0.20*yr; y2 = min(yticks)+0.12*yr; y3 = min(yticks)+0.04*yr;
-mText = ['$\sigma_{sym}=' num2str(sym.stdM(3),'%1.3g') ' \frac{rad}{m}$'];
+
+mText = ['$\sigma_{sym}=' num2str(sym.stdM(3),'%1.3g') ' \frac{rad}{m/s^2}$'];
 text(1,y1, mText, 'Interpreter','latex', 'FontSize',16)
-mText = ['$\sigma_{asym}=' num2str(asym.stdM(3),'%1.3g') ' \frac{rad}{m}$'];
+mText = ['$\sigma_{asym}=' num2str(asym.stdM(3),'%1.3g') ' \frac{rad}{m/s^2}$'];
 text(1,y2, mText, 'Interpreter','latex', 'FontSize',16)
-mText = ['$\sigma_{coco}=' num2str(coco.stdM(3),'%1.3g') ' \frac{rad}{m}$'];
+mText = ['$\sigma_{coco}=' num2str(coco.stdM(3),'%1.3g') ' \frac{rad}{m/s^2}$'];
 text(1,y3, mText, 'Interpreter','latex', 'FontSize',16)
 
+% Add labels
 xlabel('Time [ms]');
 ylabel('Moment [rad/m/s^2]');
 set(gca, 'FontSize', 16);
--- results/plotResiduals.m
+++ results/plotResiduals.m
@@ -20,6 +20,7 @@ coco.tVecEC = (0:numel(coco.G_EC(:,1))-1) * dt * 1e3;	% [ms]
 subplot(2,3,1:3);
 title('Concomitant field phase evolution', 'FontSize', 20);
 
+% Plot lines
 hold on;
 plot(sym.tVec, sym.phiCC, 'LineWidth',4);
 plot(asym.tVec, asym.phiCC, 'LineWidth',4);
@@ -29,19 +30,20 @@ plot(asym.tInv*1e3,asym.phiCC(asym.nInv),'kx');
 plot(coco.tInv*1e3,coco.phiCC(coco.nInv),'kx');
 hold off;
 
+% Add annotations
 yr = max(yticks)-min(yticks);
-mText = ['$m_{sym} = $' num2str(sym.m1*1e3,'%1.2f') '$ \frac{mT\cdot s}{m}$'];
+y1 = max(yticks)-0.04*yr; y2 = max(yticks)-0.12*yr; y3 = max(yticks)-0.20*yr;
+
+mText = ['$m_{sym} = $' num2str(sym.m1*1e6,'%1.2f') '$ \frac{\mu s \cdot T^2}{m^2}$'];
 text(sym.tInv*1e3, sym.phiCC(sym.nInv)-0.05*yr, mText, ...
 	'HorizontalAlignment','center', 'Interpreter', 'latex', 'FontSize', 16)
-mText = ['$m_{asym} = $' num2str(asym.m1*1e3,'%1.2f') '$ \frac{mT\cdot s}{m}$'];
+mText = ['$m_{asym} = $' num2str(asym.m1*1e6,'%1.2f') '$ \frac{\mu s \cdot T^2}{m^2}$'];
 text(asym.tInv*1e3, asym.phiCC(asym.nInv)-0.05*yr, mText, ...
 	'HorizontalAlignment','center', 'Interpreter', 'latex', 'FontSize', 16)
-mText = ['$m_{coco} = $' num2str(coco.m1*1e3,'%1.2f') '$ \frac{mT\cdot s}{m}$'];
+mText = ['$m_{coco} = $' num2str(coco.m1*1e6,'%1.2f') '$ \frac{\mu s \cdot T^2}{m^2}$'];
 text(coco.tInv*1e3, coco.phiCC(coco.nInv)-0.05*yr, mText, ...
 	'HorizontalAlignment','center', 'Interpreter', 'latex', 'FontSize', 16)
 
-yr = max(yticks)-min(yticks);
-y1 = max(yticks)-0.04*yr; y2 = max(yticks)-0.12*yr; y3 = max(yticks)-0.20*yr;
 mText = ['$AF_{sym}=' num2str(sym.AF_slice,'%1.2g') '_{slice} \cdot ' ...
 		num2str(sym.AF_phase,'%1.2g') '_{phase}=' num2str(sym.AF,'%1.2g') '$'];
 text(1,y1, mText, 'Interpreter','latex', 'FontSize',16)
@@ -52,15 +54,19 @@ mText = ['$AF_{coco}=' num2str(coco.AF_slice,'%1.2g') '_{slice} \cdot ' ...
 		num2str(coco.AF_phase,'%1.2g') '_{phase}=' num2str(coco.AF,'%1.2g') '$'];
 text(1,y3, mText, 'Interpreter','latex', 'FontSize',16)
 
+% Add labels
 xlabel('Time [ms]');
 ylabel('Residual phase [rad]');
 legend('\phi_{sym}','\phi_{asym}','\phi_{coco}');
 set(gca, 'FontSize', 16);
 
+
 %% Plot eddy-current G_xx
 subplot(2,3,4);
 title('Eddy-current self-term G_{xx} phase', 'FontSize', 20);
 
+
+% Plot lines
 hold on;
 plot(sym.tVecEC, sym.phiEC(:,1), 'LineWidth',4);
 plot(asym.tVecEC, asym.phiEC(:,1), 'LineWidth',4);
@@ -70,14 +76,29 @@ plot(asym.tE*1e3,asym.phiEC(asym.nE,1),'kx');
 plot(coco.tE*1e3,coco.phiEC(coco.nE,1),'kx');
 hold off;
 
+% Add annotations
+xr = max(xticks)-min(xticks); yr = max(yticks)-min(yticks);
+x = max(xticks)-0.35*xr;
+y1 = min(yticks)+0.20*yr; y2 = min(yticks)+0.12*yr; y3 = min(yticks)+0.04*yr;
+
+mText = ['$\phi _{sym}=' num2str(sym.resEC(1),'%1.3g') '\frac{rad}{m}$'];
+text(x,y1, mText, 'Interpreter','latex', 'FontSize',16)
+mText = ['$\phi _{asym}=' num2str(asym.resEC(1),'%1.3g') '\frac{rad}{m}$'];
+text(x,y2, mText, 'Interpreter','latex', 'FontSize',16)
+mText = ['$\phi _{coco}=' num2str(coco.resEC(1),'%1.3g') '\frac{rad}{m}$'];
+text(x,y3, mText, 'Interpreter','latex', 'FontSize',16)
+
+% Add labels
 xlabel('Time [ms]');
 ylabel('Residual phase [rad/m]');
 set(gca, 'FontSize', 16);
+
 
 %% Plot eddy-current G_yy
 subplot(2,3,5);
 title('Eddy-current self-term G_{yy} phase', 'FontSize', 20);
 
+% Plot lines
 hold on;
 plot(sym.tVecEC, sym.phiEC(:,2), 'LineWidth',4);
 plot(asym.tVecEC, asym.phiEC(:,2), 'LineWidth',4);
@@ -87,14 +108,29 @@ plot(asym.tE*1e3,asym.phiEC(asym.nE,2),'kx');
 plot(coco.tE*1e3,coco.phiEC(coco.nE,2),'kx');
 hold off;
 
+% Add annotations
+xr = max(xticks)-min(xticks); yr = max(yticks)-min(yticks);
+x = max(xticks)-0.35*xr;
+y1 = min(yticks)+0.20*yr; y2 = min(yticks)+0.12*yr; y3 = min(yticks)+0.04*yr;
+
+mText = ['$\phi _{sym}=' num2str(sym.resEC(2),'%1.3g') '\frac{rad}{m}$'];
+text(x,y1, mText, 'Interpreter','latex', 'FontSize',16)
+mText = ['$\phi _{asym}=' num2str(asym.resEC(2),'%1.3g') '\frac{rad}{m}$'];
+text(x,y2, mText, 'Interpreter','latex', 'FontSize',16)
+mText = ['$\phi _{coco}=' num2str(coco.resEC(2),'%1.3g') '\frac{rad}{m}$'];
+text(x,y3, mText, 'Interpreter','latex', 'FontSize',16)
+
+% Add labels
 xlabel('Time [ms]');
 ylabel('Residual phase [rad/m]');
 set(gca, 'FontSize', 16);
+
 
 %% Plot eddy-current G_zz
 subplot(2,3,6);
 title('Eddy-current self-term G_{zz} phase', 'FontSize', 20);
 
+% Plot lines
 hold on;
 plot(sym.tVecEC, sym.phiEC(:,3), 'LineWidth',4);
 plot(asym.tVecEC, asym.phiEC(:,3), 'LineWidth',4);
@@ -104,6 +140,19 @@ plot(asym.tE*1e3,asym.phiEC(asym.nE,3),'kx');
 plot(coco.tE*1e3,coco.phiEC(coco.nE,3),'kx');
 hold off;
 
+% Add annotations
+xr = max(xticks)-min(xticks); yr = max(yticks)-min(yticks);
+x = max(xticks)-0.35*xr;
+y1 = min(yticks)+0.20*yr; y2 = min(yticks)+0.12*yr; y3 = min(yticks)+0.04*yr;
+
+mText = ['$\phi _{sym}=' num2str(sym.resEC(3),'%1.3g') '\frac{rad}{m}$'];
+text(x,y1, mText, 'Interpreter','latex', 'FontSize',16)
+mText = ['$\phi _{asym}=' num2str(asym.resEC(3),'%1.3g') '\frac{rad}{m}$'];
+text(x,y2, mText, 'Interpreter','latex', 'FontSize',16)
+mText = ['$\phi _{coco}=' num2str(coco.resEC(3),'%1.3g') '\frac{rad}{m}$'];
+text(x,y3, mText, 'Interpreter','latex', 'FontSize',16)
+
+% Add labels
 xlabel('Time [ms]');
 ylabel('Residual phase [rad/m]');
 set(gca, 'FontSize', 16);
