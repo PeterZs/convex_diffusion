91eccf831611f4886d6f5c09dd3e0ef9cc9a3b05
alen-mujkanovic <alen.mujkanovic@outlook.com>
2018-10-12 12:13:30 -0400

    Added mixing time mode, added tPre

--- symmetricDiffusion.m
+++ symmetricDiffusion.m
@@ -28,21 +28,29 @@ while (b <= p.bTarget-p.bTol)	% Repeat until the b-value is large enough
 	% Create G(t) vector from lobe durations
 	switch p.MMT
 		case 0 % Monopolar pair
-			G = trapTransform([N,N],p.Gmax,nRamp,1,p.nRF,1);
+			nGap = p.nRF + max(0,p.mix*(p.nEPI-p.nPre));
+			G = trapTransform([N,N],p.Gmax,nRamp,1,nGap,1);
 		case 1 % Bipolar pair
-			G = trapTransform([N,-N,N,-N],p.Gmax,nRamp,1,p.nRF,2);
+			nGap = p.nRF + max(0,p.mix*(p.nEPI-p.nPre));
+			G = trapTransform([N,-N,N,-N],p.Gmax,nRamp,1,nGap,2);
 		case 2 % Modified bipolar pair
 			N2  = 2*N + nRamp;
-			G = trapTransform([N,-N2,-N2,N],p.Gmax,nRamp,1,p.nRF,2);
+			nGap = p.nRF + max(0,p.mix*(p.nEPI-p.nPrep));
+			G = trapTransform([N,-N2,-N2,N],p.Gmax,nRamp,1,nGap,2);
 	end
 
 	% Test b-value
 	nEnc = length(G);
-	p.inv.n = round(nEnc/2);
+	p.inv.n = round(nEnc/2) + p.mix*round((nGap-p.nRF)/2);
 	b = bValue(G, p);
 	
 	% Increment lobe duration
 	N = N + 1;
+	
+	% Check break condition
+	if (p.tMax < nEnc*dt)
+		break;
+	end
 
 end
 
@@ -52,10 +60,10 @@ sym.G = scaleGmax(G, p);
 sym.b = bValue(sym.G, p);
 
 % Lengths and durations
-sym.nPre = round(p.nRead/2);				sym.tPre = sym.nPre * dt;
+sym.nPre = max(p.nPre, not(p.mix)*p.nEPI);	sym.tPre = sym.nPre * dt;
 sym.nEnc = nEnc;							sym.tEnc = sym.nEnc * dt;
-sym.nPost = 0;								sym.tPost = sym.nPost * dt;
-sym.nInv = round(sym.nEnc/2) + sym.nPre;	sym.tInv = sym.nInv * dt;
+sym.nPost = max(0,p.nPre-p.nEPI);			sym.tPost = sym.nPost * dt;
+sym.nInv = p.inv.n + sym.nPre;				sym.tInv = sym.nInv * dt;
 sym.param.inv.n = sym.nInv;						
 sym.nE = 2 * sym.nInv;						sym.tE = sym.nE * dt;
 sym.n1 = round(sym.nEnc/2)-round(p.nRF/2);	sym.t1 = sym.n1 * dt;
@@ -69,5 +77,5 @@ sym.m2 = maxwellIndex(sym.G2,sym.param);
 sym.m = maxwellIndex(sym.G,sym.param);
 
 % Add pre-time to waveform
-sym.G = [zeros(round(p.nRead/2),1); sym.G];
+sym.G = [zeros(sym.nPre,1); sym.G; zeros(sym.nPost,1)];
 sym.n = numel(sym.G);
