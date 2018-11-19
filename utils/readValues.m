4bb01aa28921585e39cebf850a11069d52e74edc
alen-mujkanovic <alen.mujkanovic@outlook.com>
2018-11-19 15:22:10 +0100

    Updated for current data structure

--- utils/readValues.m
+++ utils/readValues.m
@@ -1,33 +1,38 @@
-function [vals] = readValues(inFolder,range,fieldName)
+function values = readValues(inFolder,range,fieldName)
 % readValues loads a specified field from each of the files in the
 % specified range of the given folder.
 %
-% inFolder      Input folder from which to read the data
-% range         Specifies which of the numbered recons to read
-% fieldName     Field of the output struct contained in the recon files
-%               e.g. 'nrSmpl,'parameters','metrics.rmse.magnitude'
+% Input:	inFolder	Input folder from which to read the data
+%			range		Specifies which of the numbered recons to read
+%			fieldName	Field of the output struct contained in the recon files
+%						e.g. 'nrSmpl,'parameters','metrics.rmse.magnitude'
 %
-% vals          Output cell array
-
-% (c) Alen Mujkanovic 2017
+% Output:	values		Output cell array
 
 
-oldFolder = cd(inFolder);           % Change to specified folder
+% Change to specified folder
+oldFolder = cd(inFolder);
+addpath(genpath(pwd));
 
-fields = strsplit(fieldName,'.');   % Split fields
-files = getfield(what, 'mat');      % Get list of all files
+% Get list of all .mat files recursively
+files = dir('**/*.mat');
+files = string({files.name})';
 k = numel(files);
-vals = cell(size(range));
+
+% Load specified fields from files in range
+fields = strsplit(fieldName,'.');	% Split fields
+values = cell(size(range));
 for i = 1:k
-    if isempty(sscanf(files{i},'%u'))
-        continue
-    elseif any(sscanf(files{i},'%u')==range(:))	% Skip unneeded files
-        val = load(files{i},fields{1});         % Get struct
-        for j = 1:numel(fields)                 % Get field
-            val = getfield(val,fields{j});
-        end
-        vals(sscanf(files{i},'%u')==range) = {val};
+    if any(i==range(:))		% Skip unneeded files
+        value = load(files{i},fields{1});
+        for j = 1:numel(fields)
+            value = getfield(value,fields{j});
+		end
+		% if ~mod(i,3), disp(files{i}); end
+        values(i==range) = {value};
     end
 end
 
-cd(oldFolder);                      % Change back to original folder
+% Change back to original folder
+rmpath(genpath(pwd));
+cd(oldFolder);
